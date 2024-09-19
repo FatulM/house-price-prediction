f11296ec778591a1659fe78ff4254db97c1a8ae7
Amirreza Madani <madani_amirreza@yahoo.com>
2024-09-19 14:32:47 +0330

    fix some issues

--- _imports.py
+++ _imports.py
@@ -43,6 +43,9 @@ from sklearn.compose import *
 from sklearn.covariance import *
 from sklearn.cross_decomposition import *
 from sklearn.datasets import *
+from sklearn.datasets.data import *
+from sklearn.datasets.images import *
+from sklearn.datasets.descr import *
 from sklearn.decomposition import *
 from sklearn.discriminant_analysis import *
 from sklearn.dummy import *
@@ -51,8 +54,11 @@ from sklearn.exceptions import *
 from sklearn.experimental import *
 from sklearn.externals import *
 from sklearn.feature_extraction import *
+from sklearn.feature_extraction.image import *
+from sklearn.feature_extraction.text import *
 from sklearn.feature_selection import *
 from sklearn.gaussian_process import *
+from sklearn.gaussian_process.kernels import *
 from sklearn.impute import *
 from sklearn.inspection import *
 from sklearn.isotonic import *
@@ -60,7 +66,7 @@ from sklearn.kernel_approximation import *
 from sklearn.kernel_ridge import *
 from sklearn.linear_model import *
 from sklearn.manifold import *
-from sklearn.metrics import *
+from sklearn.metrics.cluster import *
 from sklearn.metrics.pairwise import *
 from sklearn.mixture import *
 from sklearn.model_selection import *
@@ -76,6 +82,22 @@ from sklearn.semi_supervised import *
 from sklearn.svm import *
 from sklearn.tree import *
 from sklearn.utils import *
+from sklearn.utils.arrayfuncs import *
+from sklearn.utils.class_weight import *
+from sklearn.utils.deprecation import *
+from sklearn.utils.discovery import *
+from sklearn.utils.estimator_checks import *
+from sklearn.utils.extmath import *
+from sklearn.utils.fixes import *
+from sklearn.utils.graph import *
+from sklearn.utils.metadata_routing import *
+from sklearn.utils.metaestimators import *
+from sklearn.utils.multiclass import *
+from sklearn.utils.murmurhash import *
+from sklearn.utils.optimize import *
+from sklearn.utils.sparsefuncs import *
+from sklearn.utils.sparsefuncs_fast import *
+from sklearn.utils.stats import *
 from sklearn.utils.validation import *
 
 np.random.seed = 110
