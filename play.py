876e522486154df7bbb671d65dae62fd453de9b9
Amirreza Madani <madani_amirreza@yahoo.com>
2024-09-19 11:21:42 +0000

    fix some issues

--- play.py
+++ play.py
@@ -2,18 +2,14 @@
 
 from _imports import *
 
-# Load Meta Data:
-
-types: dict[str, str] = pd.read_csv('data/meta/types.csv', index_col=0).iloc[:, 0].to_dict()
-discrete: dict[str, list[str]] = pd.read_csv('data/meta/discrete.csv', index_col=0).iloc[:, 0] \
-    .map(lambda x: x.split(sep="|")).to_dict()
-
 # Load Data:
 
-train_X: pd.DataFrame = pd.read_csv('data/train_X.csv')
+train_X: pd.DataFrame = pd.read_csv('data/train_X_prep.csv').to_numpy()
 train_y: np.ndarray = pd.read_csv('data/train_y.csv').iloc[:, 0].to_numpy()
-test_X: pd.DataFrame = pd.read_csv('data/test_X.csv')
+test_X: pd.DataFrame = pd.read_csv('data/test_X_prep.csv').to_numpy()
 
 # TODO:
 
-print()
+print(train_X.shape)
+print(train_y.shape)
+print(test_X.shape)
